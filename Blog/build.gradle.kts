b81ccba7f480dc9d0332287200cfc250064d9bc2
Ivan Shpyakin <i.shpyakin@tinkoff.ru>
2026-01-30 15:44:06 +0300

    Add base template of spring framework project.

--- Blog/build.gradle.kts
+++ Blog/build.gradle.kts
@@ -1,15 +1,26 @@
 plugins {
     id("java")
+    id("war")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 group = "ru.yandex"
-version = "1.0-SNAPSHOT"
+version = "1.0"
+project.setProperty("archivesBaseName", "blog-app")
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework:spring-webmvc:6.2.1")
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
