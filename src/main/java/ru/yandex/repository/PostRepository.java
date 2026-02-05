7d1c40e853cfd284de3bac446bbecf917c1bfec8
Ivan Shpyakin <i.shpyakin@tinkoff.ru>
2026-02-05 12:30:15 +0300

    fix: исправлен SQL-запрос в PostRepository для корректной фильтрации по тегам и заголовку
    Изменён формирование SQL-запроса: теперь часть с JOIN и группировкой добавляется только при наличии тегов, а условие HAVING включается вместе с условием по тегам в один блок для правильной фильтрации постов. Ранее логика была разделена, что могло приводить к некорректным результатам.

--- src/main/java/ru/yandex/repository/PostRepository.java
+++ src/main/java/ru/yandex/repository/PostRepository.java
@@ -37,29 +37,31 @@ class JdbcNativePostRepository implements PostRepository {
         var tags = valuableWords.stream().filter(s -> s.startsWith("#")).map(s -> s.substring(1)).toList();
         var title = valuableWords.stream().filter(s -> !s.startsWith("#")).collect(Collectors.joining(" "));
 
-        var baseSql = "select p.id, p.title, p.text, p.likes_count, string_agg(distinct t.name, '||') as tags_list from posts p " +
-                "left join posts_tags pt on p.id = pt.post_id " +
-                "join tags t on t.id = pt.tag_id";
+        var baseSql = "select p.id, p.title, p.text, p.likes_count from posts p ";
+        if ((long) tags.size() > 0)
+        {
+            baseSql += "left join posts_tags pt on p.id = pt.post_id " +
+                    "join tags t on t.id = pt.tag_id ";
+        }
+
         if ((long) tags.size() > 0 || !title.isEmpty())
         {
             baseSql += " where ";
-        }
-        if (!title.isEmpty())
-        {
-            baseSql += "p.title like '%" + title + "%' ";
+
+            if (!title.isEmpty())
+            {
+                baseSql += "p.title like '%" + title + "%' ";
+                if ((long) tags.size() > 0)
+                {
+                    baseSql += " and ";
+                }
+            }
             if ((long) tags.size() > 0)
             {
-                baseSql += " and ";
+                baseSql += "t.name in ('" + String.join("', '", tags) + "') " +
+                        "group by p.id, p.title, p.text, p.likes_count " +
+                        "having count(distinct t.name) = " + tags.size();
             }
-        }
-        if ((long) tags.size() > 0)
-        {
-            baseSql += "t.name in ('" + String.join("', '", tags) + "') ";
-        }
-        baseSql += "group by p.id, p.title, p.text, p.likes_count";
-        if ((long) tags.size() > 0)
-        {
-            baseSql += " having count(distinct t.name) = " + tags.size();
         }
 
         baseSql += " offset " + (pageNumber - 1) * pageSize + " limit " + pageSize;
